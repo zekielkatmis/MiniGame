2c292fef765bb9dea38d9846781106b91bcfe9f0
zekielkatmis <61638692+zekielkatmis@users.noreply.github.com>
2020-03-01 03:50:26 +0300

    MiniGame

---
+++ TasKagitMakas/server.py
@@ -0,0 +1,74 @@
+import socket  # to connect server we should use socket
+from _thread import *
+import pickle
+from game import Game
+
+server = "192.168.1.112"
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+s.listen()
+print("Waiting for a connection, Server Started")
+
+connected = set()
+games = {}
+idCount = 0
+
+
+def threaded_client(conn, p, gameId):  # we wants multiple connection at once
+    global idCount
+    conn.send(str.encode(str(p)))
+
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(4096).decode()
+
+            if gameId in games:
+                game = games[gameId]
+
+                if not data:
+                    break
+                else:
+                    if data == "reset":
+                        game.resetWent()
+                    elif data != "get":
+                        game.play(p, data)
+
+                    conn.sendall(pickle.dumps(game))
+            else:
+                break
+        except:
+            break
+
+    print("Lost connection")
+    try:
+        del games[gameId]
+        print("Closing Game", gameId)
+    except:
+        pass
+    idCount -= 1
+    conn.close()
+
+
+while True:
+    conn, addr = s.accept()  # its going to accept incoming connections
+    print("Connected to:", addr)
+
+    idCount += 1  # how many people connect to the server
+    p = 0
+    gameId = (idCount - 1) // 2
+    if idCount % 2 == 1:  # if online player number became odd number it creates a new game
+        games[gameId] = Game(gameId)
+        print("Creating a new game...")
+    else:
+        games[gameId].ready = True
+        p = 1
+
+    start_new_thread(threaded_client, (conn, p, gameId))
